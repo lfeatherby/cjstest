df0f5f042328c4e942b1a2dac9ed5f6437a5aebd
lfeatherby <larissafeatherby@gmail.com>
2024-04-11 11:50:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log("tests2");
+import Color from "https://colorjs.io/dist/color.js";
 let green = new Color("lch", [80, 90, 120]);
 let cssColor = green.display();
 cssColor.color.inGamut();
