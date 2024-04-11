bd6ea43a0b1d8cb87533051b4560f70a7f068909
lfeatherby <larissafeatherby@gmail.com>
2024-04-11 12:29:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let green = new Color("srgb", [0, 0.9, 0]);
 let cssColor = green.display();
 cssColor.color.inGamut();
 console.log(green.toString());
-document.getElementById("testt").style.color = green.toString();
+document.getElementById("test").style.color = green.toString();
