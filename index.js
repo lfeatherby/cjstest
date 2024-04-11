1d25d31d6876753522393f7696708a84cdbf2bbe
lfeatherby <larissafeatherby@gmail.com>
2024-04-11 12:19:57 -0400

    Updated script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("testing");
-import Color from "https://colorjs.io/dist/color.js";
-let green = new Color("lch", [80, 90, 120]);
+let green = new Color("srgb", [0, 0.9, 0]);
 let cssColor = green.display();
 cssColor.color.inGamut();
+console.log(green.toString());
+document.getElementById("testt").style.color = green.toString();
